2933901830994f2f7726773b9c21a6351ffa44b1
rpidanny <abhishekmaharjan1993@gmail.com>
2018-02-04 20:15:48 +0545

    ..

--- api/amazon/handlers.js
+++ api/amazon/handlers.js
@@ -1,12 +1,12 @@
 const logger = require('../../system').logger;
 
 const handlers = {
-  'SensorIntent' : function() {
+  SensorIntent : function() {
     logger.info(this.event);
     //emit response directly
     this.emit(':tell', 'Hello World!');
   },
-  'DeviceIntent' : function() {
+  DeviceIntent : function() {
     logger.info(this.event);
     this.emit(':tell', 'Hello World from device');
   }
